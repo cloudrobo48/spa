3d1356c184e347b382b04130803288ef389a5714
cloudrobo48 <hatakazuhiro.kyoto@gmail.com>
2025-10-27 21:58:17 +0900

    update environment

--- backend.tf
+++ backend.tf
@@ -13,8 +13,8 @@ terraform {
   }
   backend "s3" {
     bucket = "terraform-statefile-20251021"
-    key    = "terraform/state.tfstate"
-    #    key            = "terraform/${terraform.workspace}/state.tfstate"
+    #    key    = "terraform/state.tfstate"
+    key            = "terraform/${terraform.workspace}/state.tfstate"
     region         = "ap-northeast-1"
     dynamodb_table = "Terraform_LockTable_20251021"
     encrypt        = true
